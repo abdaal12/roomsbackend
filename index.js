8caa1ad74e96a4743ca3d4dfb7e40c53a4ad0551
Abdaal Ali <136990423+abdaal12@users.noreply.github.com>
2026-09-10 15:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const leadRoutes     = require('./routes/leads');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://rooms-amber-xi.vercel.app/',
+    // Add any other domains here
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
